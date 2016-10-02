87a65c183472d84a3b9b35b96a936087906ad856
cthang94 <cthang94@gmail.com>
2016-10-02 06:09:15 -0400

    Add files via upload

--- problemSet1.py
+++ problemSet1.py
@@ -1,20 +1,18 @@
-try:
-    outstandingBalance = float(raw_input("Please enter your outstanding balance: "))
-    annualInterestRate = float(raw_input("Please enter the annual credit card interest rate as a decimal: "))
-    minimumMonthlyPaymentRate = float(raw_input("Please enter the minimum monthly payment rate as a decimal: "))
-    month = 0
-    while month < 13:
-        minimumMonthlyPayment = (minimumMonthlyPaymentRate * outstandingBalance)
-        interestPaid = (annualInterestRate / 12 * outstandingBalance)
-        principalPaid = (minimumMonthlyPayment - interestPaid)
-        outstandingBalance = (outstandingBalance - principalPaid)
-        
-        print("Month: ", month)
-        print("Minimum monthly payment: ", minimumMonthlyPayment)
-        print("Principal paid: ", principalPaid)
-        print("Remaining balance: ", outstandingBalance)
-        month = month + 1
 
-except:
-    print("Not a number!")
+outstandingBalance = float(input("Please enter your outstanding balance: "))
+annualInterestRate = float(input("Please enter the annual credit card interest rate as a decimal: "))
+minimumMonthlyPaymentRate = float(input("Please enter the minimum monthly payment rate as a decimal: "))
+month = 0
+while month < 13:
+    minimumMonthlyPayment = (minimumMonthlyPaymentRate * outstandingBalance)
+    interestPaid = (annualInterestRate / 12 * outstandingBalance)
+    principalPaid = (minimumMonthlyPayment - interestPaid)
+    outstandingBalance = (outstandingBalance - principalPaid)
+        
+    print("Month: ", month)
+    print("Minimum monthly payment: ", minimumMonthlyPayment)
+    print("Principal paid: ", principalPaid)
+    print("Remaining balance: ", outstandingBalance)
+    month = month + 1
+
 
